3cc5b8dd3787c049334ccd720725942c57760547
MateusDebut <mateusdossantosribeiro100@usp.br>
2022-06-02 01:08:24 -0300

    demonstra

---
+++ operacoes_aritmeticas/operations.hs
@@ -0,0 +1,10 @@
+main = do
+    print (1+1) --soma
+    print ((+) 3 2) -- outra forma de fazer soma, vale para as outras operações
+
+    print (2*3) --multiplicação
+    print(3-2) -- subtração
+    print(6/3) -- divisão
+    print (mod 12 5) -- resto da divisão
+    print (abs (-8)) -- módulo. Valor absoluto
+    print (negate 9) -- inverte o sinal
